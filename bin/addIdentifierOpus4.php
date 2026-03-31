0594b7349ce2933d6943db82e3c87fa818890e6c
paulborchert <paul.borchert@gbv.de>
2026-03-31 17:46:32 +0200

    REP-1083: skip enrichment and filtering when logline has errors
    Add hasErrors() method to ePuStaLogline and use it in addIdentifierMIR,
    addIdentifierOpus4 and filter to prevent processing of erroneous log lines.

--- bin/addIdentifierOpus4.php
+++ bin/addIdentifierOpus4.php
@@ -29,7 +29,9 @@ while (!feof(STDIN)) {
     if ($line = trim(fgets(STDIN))) {
         $logline = new epusta\ePuStaLogline();
         if ($epuStaLoglineParser->parse($line, $logline)) {
-            $opusToolbox->addIdentifier($logline, $prefix);
+            if (!$logline->hasErrors()) {
+                $opusToolbox->addIdentifier($logline, $prefix);
+            }
             echo($logline . "\n");
         } else {
             // die("Error: malformed Logline" . $line . "\n")
